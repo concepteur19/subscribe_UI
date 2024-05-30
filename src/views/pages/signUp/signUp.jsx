b3f7e0eb93aa24239eccb05a2fef073e1708dd18
ChristianLudovic <talchrist10@gmail.com>
2024-05-30 16:52:41 +0300

    login page

--- src/views/pages/signUp/signUp.jsx
+++ src/views/pages/signUp/signUp.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 export function SignUp() {
-    const navigate = useNavigate();
     
     const [data, setData] = useState({
         username: '',
