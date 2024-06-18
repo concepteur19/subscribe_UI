063e40f38e874761233dfe051ba4fbdcfea44c7e
concepteur19 <zobel.tchomgui@gmail.com>
2024-06-18 21:10:11 +0200

    update payment card

--- src/views/components/basis/home_basis/card-latest-payment.tsx
+++ src/views/components/basis/home_basis/card-latest-payment.tsx
@@ -1,19 +1,33 @@
 import React from "react";
-import CardApproved from "./card-approved";
+import CardStatus from "./card-approved";
 
-const CardLatestPayment = () => {
-  
-  
+// Définir les types des props
+interface CardLatestPaymentProps {
+  amount: number;
+  paymentDate: string;
+  isApproved: boolean;
+}
+
+const CardLatestPayment: React.FC<CardLatestPaymentProps> = ({
+  amount,
+  paymentDate,
+  isApproved,
+}) => {
   return (
-    <div className=" text-sm rounded-xl bg-[#0B0B1A] py-[14.5px] px-2 lg:px-4 flex justify-around items-center ">
-      <div className="flex space-x-2 ">
-        <span className=" text-[#fff]">$24.00</span>
-        <span className=" ">USD</span>
+    <div className="text-sm rounded-xl bg-[#0B0B1A] py-[14.5px]  px-2 lg:px-4 flex justify-around items-center">
+      {/* Affichage du montant et de la devise */}
+      <div className="flex space-x-2 w-[23%] ">
+        <span className="text-[#fff]">${amount.toFixed(2)}</span>
+        <span>USD</span>
       </div>
-      <div className=" ">
-        <CardApproved />
+
+      {/* Affichage de l'état de l'approbation */}
+      <div className="w-[23%] flex justify-center items-center ">
+        <CardStatus isApproved={isApproved} /> 
       </div>
-      <span className="card">2 May, 2023</span>
+
+      {/* Affichage de la date de paiement */}
+      <div className="card w-[23%]  flex justify-end items-center">{paymentDate}</div>
     </div>
   );
 };
